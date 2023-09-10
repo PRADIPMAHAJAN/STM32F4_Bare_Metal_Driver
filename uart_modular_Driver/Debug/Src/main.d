51178b978e9c9d022227171d8a97ec704c6dfd3b
PRADIPMAHAJAN <mahajanpradip031@gmail.com>
2023-09-10 11:41:31 +0530

    UART.h include and main use this

---
+++ uart_modular_Driver/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/core_cm4.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_version.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_compiler.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_gcc.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/mpu_armv7.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/uart.h
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/core_cm4.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_version.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_compiler.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_gcc.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/mpu_armv7.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/uart.h:
---
+++ uart_modular_Driver/Debug/Src/uart.d
@@ -0,0 +1,18 @@
+Src/uart.o: ../Src/uart.c ../Inc/uart.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/core_cm4.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_version.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_compiler.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_gcc.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/mpu_armv7.h \
+ E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/uart.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/core_cm4.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_version.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_compiler.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/cmsis_gcc.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Include/mpu_armv7.h:
+E:/Udemy_STM32F4-BareMetal_Workspace/7_uart_modular/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
